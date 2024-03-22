86e373a48506d476f6ae7d2dd927704120701f30
Camilo Fuentes <camilo.fuentes.01.03.20@gmail.com>
2024-03-21 22:02:27 -0300

    Figuras Listas (Luis)

--- src/Graficador.java
+++ src/Graficador.java
@@ -19,7 +19,6 @@ public class Graficador extends JFrame {
     private JButton limpiarButton;
 
     public Graficador(){
-
         setContentPane(panel);
         setSize(500, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -67,6 +66,60 @@ public class Graficador extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+            }
+        });
+        //Dibujar Flechita -->
+        lineaDeFlujoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Graphics g = panel1.getGraphics();
+                g.setColor(Color.BLACK);
+                g.drawLine(200,200,200, 100);
+                g.drawLine(200,200,225,175);
+                g.drawLine(200,200,175,175);
+            }
+        });
+        //Dibujar Paralelogramo /__/
+        entradaSalidaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Graphics g = panel1.getGraphics();
+                g.setColor(Color.orange);
+                g.drawLine(125,150,250, 150);
+                g.drawLine(250,150,225,200);
+                g.drawLine(225,200,100,200);
+                g.drawLine(100,200,125,150);
+            }
+        });
+        //Dibujar Impresion /_-/
+        documentoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                QuadCurve2D curve = new QuadCurve2D.Double();
+                Graphics2D lapiz = (Graphics2D) panel1.getGraphics();
+                lapiz.setColor(Color.CYAN);
+                lapiz.drawLine(100,100,200,100);
+                lapiz.drawLine(200,100,200,140);
+                lapiz.drawLine(100,100,100,150);
+                curve.setCurve(100,150,125,165,150,150);
+                lapiz.draw(curve);
+                curve.setCurve(150,150,175,135,200,140);
+                lapiz.draw(curve);
+            }
+        });
+        //Dibujar Rectangulo curvo (__)
+        inicioFinButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                QuadCurve2D curve = new QuadCurve2D.Double();
+                Graphics2D lapiz = (Graphics2D) panel1.getGraphics();
+                lapiz.setColor(Color.MAGENTA);
+                lapiz.drawLine(50,200,150,200);
+                lapiz.drawLine(50,250,150,250);
+                curve.setCurve(50,200,25,225,50,250);
+                lapiz.draw(curve);
+                curve.setCurve(150,200,175,225,150,250);
+                lapiz.draw(curve);
             }
         });
     }
