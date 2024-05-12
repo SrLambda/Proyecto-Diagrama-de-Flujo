6ad662eb1c47741956881f0648302f5c22b9b273
Camilo Fuentes <59295286+SrLambda@users.noreply.github.com>
2024-05-12 19:31:38 -0400

    no me deja hacer pull :C

--- src/Dibujos/PanelesMovibles/Decision/DibujoDecisionInterno.java
+++ src/Dibujos/PanelesMovibles/Decision/DibujoDecisionInterno.java
@@ -1,13 +1,9 @@
 package Dibujos.PanelesMovibles.Decision;
 
 import Dibujos.PanelPersonalizado;
-import Dibujos.PanelesMovibles.DibujoDecision;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.List;
 
