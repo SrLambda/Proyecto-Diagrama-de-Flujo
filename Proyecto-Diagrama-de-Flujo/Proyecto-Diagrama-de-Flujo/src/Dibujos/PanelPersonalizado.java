aa3e808509a80d4610333aba3e19cfbad5ce6e6a
Catalina Herrera <catherreraa@gmail.com>
2024-05-13 22:41:11 -0400

    Fix bugs: al eliminar una figura al while, se agregaba una línea horizontal (pero ya no uwu)

---
+++ Proyecto-Diagrama-de-Flujo/Proyecto-Diagrama-de-Flujo/src/Dibujos/PanelPersonalizado.java
@@ -0,0 +1,17 @@
+package Dibujos;
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelPersonalizado extends JPanel
+{
+
+    protected String texto;
+
+
+    public PanelPersonalizado(String texto) {
+
+        this.texto = texto;
+        setPreferredSize(new Dimension(100, 50));
+    }
+
+}
---
+++ Proyecto-Diagrama-de-Flujo/Proyecto-Diagrama-de-Flujo/src/Front.java
@@ -0,0 +1,181 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.QuadCurve2D;
+
+public class Front extends JFrame {
+
+    private JPanel panel1;
+    private JPanel panel;
+    private JButton inicioButton;
+    private JButton etapaDelProcesoButton;
+    private JButton decisionButton;
+    private JButton entradaButton;
+    private JButton salidaButton;
+    private JButton documentoButton;
+    private JButton limpiarButton;
+    private JButton FinButton;
+    private JPanel columna;
+
+
+    public Front(Controlador controlador)
+    {
+
+        setContentPane(panel);
+        setSize(700, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Graficador Interactivo de Diagramas de flujo");
+        setVisible(true);
+
+
+        this.columna = new JPanel();
+        controlador.initFront(Front.this);
+
+
+
+
+        //Point2D punto1 = new Point2D.Double(10, 10);
+        //Line2D linea1 = new Line2D.Double(50, 50, 150, 150);
+        //QuadCurve2D curvaCuadratica1 = new QuadCurve2D.Double(50, 50, 100, 150, 150, 50);
+
+
+        //botón para dibujar un rectángulo
+        etapaDelProcesoButton.addActionListener(new ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+
+                controlador.crearProceso(Front.this);
+            }
+
+        });
+
+
+        //boton para dibujar un rombo
+        decisionButton.addActionListener(new ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+
+                controlador.crearDecision(Front.this);
+
+            }
+
+        });
+
+
+        limpiarButton.addActionListener(new ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                controlador.limpiarPantalla(Front.this);
+            }
+
+        });
+
+
+        /*entradaSalidaButton.addActionListener(new ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+
+            }
+
+        });*/
+
+        //Dibujar Paralelogramo /__/
+        entradaButton.addActionListener(new ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+
+                controlador.crearEntreda(Front.this);
+
+            }
+
+        });
+
+
+
+        //Dibujar Paralelogramo /__/
+        salidaButton.addActionListener(new ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+
+                controlador.crearSalida(Front.this);
+
+            }
+
+        });
+
+
+        //Dibujar Impresion /_-/
+        documentoButton.addActionListener(new ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+
+                controlador.crearImpresion(Front.this);
+
+            }
+
+        });
+
+
+        //Dibujar Rectangulo curvo (__)
+        inicioButton.addActionListener(new ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+
+                controlador.crearInicio(Front.this);
+            }
+
+        });
+
+        FinButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+
+                controlador.crearFin(Front.this);
+
+            }
+
+        });
+
+    }
+
+    public JPanel getPanel1()
+    {
+
+        return panel1;
+
+    }
+
+    public JPanel getColumna()
+    {
+
+        return columna;
+
+    }
+}
