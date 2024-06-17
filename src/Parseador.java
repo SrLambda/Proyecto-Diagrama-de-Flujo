caaee27e32c30f3cb6dd9dc0137ee6eeb6a1c1dc
SRLAMBDA <camilo.fuentes.01.03.20@gmail.com>
2024-06-17 17:14:51 -0400

    Se me olvidó el commit, inicio de compilador

--- src/Parseador.java
+++ src/Parseador.java
@@ -4,15 +4,18 @@ import Dibujos.PanelesMovibles.*;
 import java.util.List;
 
 public class Parseador {
+
     private List<PanelPersonalizado> componentes;
-    private StringBuilder pseudo_codigo;
+    private StringBuilder            pseudo_codigo;
+    private StringBuilder            cod_phython;
 
 
     Parseador(List<PanelPersonalizado> _componenetes)
     {
 
-        this.componentes = _componenetes;
+        this.componentes   = _componenetes;
         this.pseudo_codigo = new StringBuilder();
+        this.cod_phython   = new StringBuilder();
 
 
         // PSEUDO CODIGO INIT
@@ -27,7 +30,7 @@ public class Parseador {
 
         this.pseudo_codigo.append("INICIO\n");
 
-        this.pseudo_codigo.append(pseudoCodIntermedio(0,this.componentes));
+        this.pseudoCodIntermedio(0,this.componentes);
 
         this.pseudo_codigo.append("FIN");
     }
@@ -86,9 +89,7 @@ public class Parseador {
 
 
 
-    private String pseudoCodIntermedio(int tab,List<PanelPersonalizado> lista){
-
-        StringBuilder cod_inter = new StringBuilder();
+    private void pseudoCodIntermedio(int tab,List<PanelPersonalizado> lista){
 
         for(PanelPersonalizado componente : lista){
 
@@ -103,8 +104,9 @@ public class Parseador {
 
         }
 
-        return cod_inter.toString();
     }
+
+    // PSEUDOCODIGO
 
 
     private void agregarLinea(String tipo,PanelPersonalizado componente,int tab){
@@ -161,8 +163,8 @@ public class Parseador {
                 // Verdad
                 this.pseudo_codigo.append(separacion).append("{\n");
 
-                String verdad = pseudoCodIntermedio(tab+1,aux.getVerdad());
-                this.pseudo_codigo.append(verdad);
+                pseudoCodIntermedio(tab+1,aux.getVerdad());
+
 
                 this.pseudo_codigo.append(separacion).append("}\n");
 
@@ -171,8 +173,7 @@ public class Parseador {
                 this.pseudo_codigo.append(separacion).append("ENTONCES\n");
                 this.pseudo_codigo.append(separacion).append("{\n");
 
-                String falso = pseudoCodIntermedio(tab+1,aux.getFalso());
-                this.pseudo_codigo.append(falso);
+                pseudoCodIntermedio(tab+1,aux.getFalso());
 
                 this.pseudo_codigo.append(separacion).append("}\n");
                 break;
@@ -187,8 +188,8 @@ public class Parseador {
 
                 this.pseudo_codigo.append(separacion).append("{\n");
 
-                String ciclo_while = pseudoCodIntermedio(tab+1,aux_w.getLista());
-                this.pseudo_codigo.append(ciclo_while);
+                pseudoCodIntermedio(tab+1,aux_w.getLista());
+
 
                 this.pseudo_codigo.append(separacion).append("}\n");
 
@@ -208,8 +209,7 @@ public class Parseador {
 
                 this.pseudo_codigo.append(separacion).append("{\n");
 
-                String ciclo_for = pseudoCodIntermedio(tab+1,aux_f.getLista());
-                this.pseudo_codigo.append(ciclo_for);
+                pseudoCodIntermedio(tab+1,aux_f.getLista());
 
                 this.pseudo_codigo.append(separacion).append("}\n");
 
@@ -225,8 +225,173 @@ public class Parseador {
 
                 this.pseudo_codigo.append(separacion).append("{\n");
 
-                String ciclo_doWhile = pseudoCodIntermedio(tab+1,aux_dw.getLista());
-                this.pseudo_codigo.append(ciclo_doWhile);
+                pseudoCodIntermedio(tab+1,aux_dw.getLista());
+
+
+                this.pseudo_codigo.append(separacion).append("}\n");
+
+                this.pseudo_codigo.append(separacion).append("MIENTRAS ( ");
+                this.pseudo_codigo.append(componente.getTexto());
+                this.pseudo_codigo.append(" )\n");
+
+                break;
+        }
+    }
+
+
+
+    //Ejecutable
+
+    public String generarEjecutable()
+    {
+        this.cod_phython   = new StringBuilder();
+
+        this.codigo(0,this.componentes);
+
+        return  this.cod_phython.toString();
+    }
+
+
+    public void codigo(int tab,List<PanelPersonalizado> lista)
+    {
+
+        for(PanelPersonalizado componente : lista){
+
+            //Identificar componente
+            String tipo = identificarComponente(componente);
+
+            //Añadir componenete
+            if(!tipo.equals("---"))
+            {
+                agregarInstruccion(tipo,componente,tab);
+            }
+
+        }
+
+    }
+
+
+    public void agregarInstruccion(String tipo,PanelPersonalizado componente,int tab)
+    {
+
+        StringBuilder separacion = new StringBuilder();
+
+        for (int i = 0; i < tab; i++) {
+            separacion.append("\t");
+        }
+
+
+        switch (tipo){
+            case "Proceso":
+
+                this.cod_phython.append(separacion);
+                //Hay que modificar
+                this.cod_phython.append(componente.getTexto());
+
+                this.cod_phython.append("\n");
+
+                break;
+
+            case "Entrada":
+
+                this.cod_phython.append(separacion).append(componente.getTexto());
+                this.cod_phython.append(" = ");
+                this.cod_phython.append("\n");
+
+                break;
+
+            case "Salida":
+
+                this.pseudo_codigo.append(separacion).append("SALIDA ==> ");
+                this.pseudo_codigo.append(componente.getTexto());
+                this.pseudo_codigo.append("\n");
+
+                break;
+
+            case "Documento":
+
+                this.pseudo_codigo.append(separacion).append("IMPRIMIR ==> ");
+                this.pseudo_codigo.append(componente.getTexto());
+                this.pseudo_codigo.append("\n");
+
+                break;
+
+            case "Decision":
+
+                DibujoDecision aux = (DibujoDecision) componente;
+
+                // Condicion
+                this.pseudo_codigo.append(separacion).append("SI ( ");
+                this.pseudo_codigo.append(componente.getTexto());
+                this.pseudo_codigo.append(" )\n");
+
+                // Verdad
+                this.pseudo_codigo.append(separacion).append("{\n");
+
+                pseudoCodIntermedio(tab+1,aux.getVerdad());
+
+
+                this.pseudo_codigo.append(separacion).append("}\n");
+
+
+                // Falso
+                this.pseudo_codigo.append(separacion).append("ENTONCES\n");
+                this.pseudo_codigo.append(separacion).append("{\n");
+
+                pseudoCodIntermedio(tab+1,aux.getFalso());
+
+                this.pseudo_codigo.append(separacion).append("}\n");
+                break;
+
+            case "While":
+
+                DibujoWhile aux_w = (DibujoWhile) componente;
+
+                this.pseudo_codigo.append(separacion).append("MIENTRAS ( ");
+                this.pseudo_codigo.append(componente.getTexto());
+                this.pseudo_codigo.append(" )\n");
+
+                this.pseudo_codigo.append(separacion).append("{\n");
+
+                pseudoCodIntermedio(tab+1,aux_w.getLista());
+
+
+                this.pseudo_codigo.append(separacion).append("}\n");
+
+                break;
+
+            case "For":
+
+                DibujoFor aux_f = (DibujoFor) componente;
+
+                int[]  aux_val = aux_f.getIntervalo();
+                String var     = aux_f.getTexto();
+
+                this.pseudo_codigo.append(separacion).append("PARA ( ");
+                this.pseudo_codigo.append(var).append(" = ").append(aux_val[0]).append(" ; ");
+                this.pseudo_codigo.append(var).append(" += ").append(aux_val[1]).append(" ; ");
+                this.pseudo_codigo.append(var).append(" == ").append(aux_val[2]).append(" )\n");
+
+                this.pseudo_codigo.append(separacion).append("{\n");
+
+                pseudoCodIntermedio(tab+1,aux_f.getLista());
+
+                this.pseudo_codigo.append(separacion).append("}\n");
+
+
+                break;
+
+            case "Do-While":
+
+                DibujoDoWhile aux_dw = (DibujoDoWhile) componente;
+
+                this.pseudo_codigo.append(separacion).append("HACER\n");
+
+
+                this.pseudo_codigo.append(separacion).append("{\n");
+
+                pseudoCodIntermedio(tab+1,aux_dw.getLista());
+
 
                 this.pseudo_codigo.append(separacion).append("}\n");
 
